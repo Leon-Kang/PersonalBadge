7738896d50166bcc599455f532ff794a7c1a04bb
Leon-Kang <coderleonkang@gmail.com>
2018-12-23 14:38:46 +0800

    Initial Commit

---
+++ PersonalBadge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PersonalBadge
+//
+//  Created by 康梁 on 2018/12/23.
+//  Copyright © 2018 LeonKang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
